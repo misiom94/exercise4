a62d1e6d36245e249f0f746e16a2e3cc29bbbec1
misiom94 <misiom94@gmail.com>
2017-05-03 20:22:21 +0200

    Homework ActiveJMS

--- src/main/java/wdsr/exercise4/receiver/JmsQueueReceiver.java
+++ src/main/java/wdsr/exercise4/receiver/JmsQueueReceiver.java
@@ -1,9 +1,22 @@
 package wdsr.exercise4.receiver;
 
+import java.math.BigDecimal;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import wdsr.exercise4.sender.JmsSender;
+import wdsr.exercise4.PriceAlert;
+import wdsr.exercise4.VolumeAlert;
 
 /**
  * TODO Complete this class so that it consumes messages from the given queue and invokes the registered callback when an alert is received.
@@ -12,29 +25,97 @@ import wdsr.exercise4.sender.JmsSender;
  */
 public class JmsQueueReceiver {
 	private static final Logger log = LoggerFactory.getLogger(JmsQueueReceiver.class);
+	private final String connectionUri = "tcp://localhost:62616";
+	private ActiveMQConnectionFactory connectionFactory; 
+    private Connection connection; 
+    private Session session; 
+    private Destination destination; 
+    private AlertService alertService;
+	
 	
 	/**
 	 * Creates this object
 	 * @param queueName Name of the queue to consume messages from.
 	 */
 	public JmsQueueReceiver(final String queueName) {
-		// TODO
+		connectionFactory = new ActiveMQConnectionFactory(connectionUri); 
+		connectionFactory.setTrustAllPackages(true);
+        try {
+			connection = connectionFactory.createConnection();
+			connection.start(); 
+	        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE); 
+	        destination = session.createQueue(queueName);
+		} catch (JMSException e) {
+			log.info("JmsQueueReciever Error !");
+			e.printStackTrace();
+		} 
 	}
 
 	/**
 	 * Registers the provided callback. The callback will be invoked when a price or volume alert is consumed from the queue.
 	 * @param alertService Callback to be registered.
 	 */
+	
 	public void registerCallback(AlertService alertService) {
-		// TODO
+		 try {
+				MessageConsumer consumer = session.createConsumer(destination);
+				this.alertService = alertService;
+				consumer.setMessageListener(new EventListener()); 
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
+	}
+	public class EventListener implements MessageListener{
+		@Override
+		public void onMessage(Message message) { 
+		
+			if(message instanceof ObjectMessage){
+				ObjectMessage objectMessage = (ObjectMessage) message;
+				runServiceAllert(objectMessage);
+			}else if(message instanceof TextMessage){
+				TextMessage textMessage = (TextMessage) message;
+				runServiceAllert(textMessage);
+			}
+		
 	}
 	
-	/**
-	 * Deregisters all consumers and closes the connection to JMS broker.
-	 */
-	public void shutdown() {
-		// TODO
+	private void runServiceAllert(ObjectMessage objectMessage){	
+		try {
+				if(objectMessage.getJMSType().equals("PriceAlert")){
+					PriceAlert priceAlert = (PriceAlert) objectMessage.getObject();
+					alertService.processPriceAlert(priceAlert);
+				}else if(objectMessage.getJMSType().equals("VolumeAlert")){
+					VolumeAlert volumeAlert = (VolumeAlert) objectMessage.getObject();
+					alertService.processVolumeAlert(volumeAlert);
+				}
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
 	}
+	
+	private void runServiceAllert(TextMessage textMessage){
+		try {
+			String text = textMessage.getText();
+			String[] splited = text.split("\\r?\\n");
+			String[] result = new String[3];
+			for (int i = 0; i < splited.length; i++) {
+				result[i] = splited[i].substring(splited[i].indexOf('=') + 1).trim();	
+			}
+			String[] values = result; 
+			String type = textMessage.getJMSType();
+			
+			if(type.equals("PriceAlert")){
+				alertService.processPriceAlert(new PriceAlert(Long.valueOf(values[0]), values[1], new BigDecimal(values[2])));
+			}else if(type.equals("VolumeAlert")){
+				alertService.processVolumeAlert(new VolumeAlert(Long.valueOf(values[0]), values[1], Long.valueOf(values[2])));
+			}
+			
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 
 	// TODO
 	// This object should start consuming messages when registerCallback method is invoked.
@@ -56,4 +137,15 @@ public class JmsQueueReceiver {
 	//		Volume=<long value>
 	
 	// When shutdown() method is invoked on this object it should remove the listeners and close open connection to the broker.   
+}
+	public void shutdown() {
+		if (connection != null) { 
+            try {
+				connection.close();
+			} catch (JMSException e) {
+				e.printStackTrace();
+			} 
+        } 
+		
+	}
 }
--- src/main/java/wdsr/exercise4/sender/JmsSender.java
+++ src/main/java/wdsr/exercise4/sender/JmsSender.java
@@ -3,6 +3,17 @@ package wdsr.exercise4.sender;
 import java.math.BigDecimal;
 import java.util.Map;
 
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.jms.MessageProducer;
+import javax.jms.ObjectMessage;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import wdsr.exercise4.Order;
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,10 +22,26 @@ public class JmsSender {
 	
 	private final String queueName;
 	private final String topicName;
-
+	
+	private Session session;
+	private ActiveMQConnectionFactory connectionFactory; 
+	private Connection connection;
+	private final String connectionUri = "tcp://localhost:62616";
+	
+	public void createConnection() throws JMSException{
+		connectionFactory = new ActiveMQConnectionFactory(connectionUri); 
+		connection = connectionFactory.createConnection();
+		session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE); 
+	}
+	
 	public JmsSender(final String queueName, final String topicName) {
 		this.queueName = queueName;
 		this.topicName = topicName;
+		try {
+			createConnection();
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -24,7 +51,22 @@ public class JmsSender {
 	 * @param price Price of the product
 	 */
 	public void sendOrderToQueue(final int orderId, final String product, final BigDecimal price) {
-		// TODO
+		try {
+			Destination destination = session.createQueue(queueName);
+			MessageProducer messageProducer = session.createProducer(destination);
+			Order orderToSend = new Order(orderId, product, price);
+			ObjectMessage messageToSend = session.createObjectMessage();
+			messageToSend.setObject(orderToSend);
+			messageToSend.setJMSType("Order");
+			messageToSend.setStringProperty("WDSR-System", "OrderProcessor");
+			messageProducer.send(messageToSend);
+			session.close();
+			connection.close();
+			log.info("Message send !");
+		} catch (JMSException e) {
+			e.printStackTrace();
+			log.info("Error while sending message !");
+		}
 	}
 
 	/**
@@ -32,7 +74,21 @@ public class JmsSender {
 	 * @param text String to be sent
 	 */
 	public void sendTextToQueue(String text) {
-		// TODO
+		try {
+			Destination destination = session.createQueue(queueName);
+			MessageProducer producer = session.createProducer(destination);
+			TextMessage textMessage = session.createTextMessage();
+			textMessage.setText(text);
+			textMessage.setJMSType("Order");
+			textMessage.setStringProperty("WDSR-System", "OrderProcessor");
+			producer.send(textMessage);
+			session.close();
+			connection.close();
+			log.info("textMessage was send succesfuly!");
+		} catch (JMSException e) {
+			e.printStackTrace();
+			log.info("ERROR while sending textmessage!");
+		}
 	}
 
 	/**
@@ -40,6 +96,20 @@ public class JmsSender {
 	 * @param map Map of key-value pairs to be sent.
 	 */
 	public void sendMapToTopic(Map<String, String> map) {
-		// TODO
+		try {
+			Destination destination = session.createTopic(topicName);
+			MessageProducer messageProducer = session.createProducer(destination);
+			MapMessage mapMessage = session.createMapMessage();
+			for (Map.Entry<String, String> entryMap : map.entrySet()) {
+				mapMessage.setString(entryMap.getKey(), entryMap.getValue());
+			}
+			messageProducer.send(mapMessage);
+			session.close();
+			connection.close();
+			log.info("mapMessage was send succesfuly!");
+		} catch (JMSException e) {
+			e.printStackTrace();
+			log.info("ERROR while sending mapMessage!");
+		}
 	}
 }
